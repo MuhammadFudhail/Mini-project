44542f65d2586d0625fbbd5f3bfbdb5842c9975a
Muhamad Fudhail <136212616+MuhammadFudhail@users.noreply.github.com>
2024-11-14 20:17:40 +0800

    membuat halaman create

--- src/Dashboard/Add-item/index.jsx
+++ src/Dashboard/Add-item/index.jsx
@@ -1,11 +1,37 @@
-import { AnvilIcon, BadgePercent, ImageUpIcon, MapPinned, PackageOpenIcon, PackageSearchIcon, SquareArrowLeft } from 'lucide-react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import {
+  AnvilIcon,
+  BadgePercent,
+  ImageUpIcon,
+  MapPinned,
+  PackageOpenIcon,
+  PackageSearchIcon,
+  SquareArrowLeft
+} from 'lucide-react';
 
 const AddItem = () => {
   const [image, setImage] = useState(null);
-  const [description, setDescription] = useState (null);
+  const [name, setName] = useState('');
+  const [type, setType] = useState('');
+  const [weight, setWeight] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [address, setAddress] = useState('');
+  const [products, setProducts] = useState([]);
+  const API_URL = 'https://6718b4807fc4c5ff8f4aabe4.mockapi.io/products';
+  const navigate = useNavigate();
 
-  // Fungsi untuk menangani upload gambar
+  const alert = () => {
+    Swal.fire({
+      title: "Data berhasil ditambahkan",
+      text: "You clicked the button!",
+      icon: "success"
+    });
+  };
+
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -14,18 +40,54 @@ const AddItem = () => {
     }
   };
 
+  const handleClick = () => {
+    alert();
+    tambahData();
+  };
+
+  const tambahData = async () => {
+    try {
+      const newProduct = {
+        name,
+        type,
+        weight,
+        price,
+        description,
+        address,
+        image,
+      };
+      await axios.post(API_URL, newProduct);
+      navigate("/");
+      fetchData();
+    } catch (error) {
+      console.error('Gagal menambahkan data:', error);
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Gagal mengambil data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div className='flex justify-between items-center mx-7 mt-8'>
         <h1 className='font-medium text-xl'>Tambah Data Produk</h1>
-        <SquareArrowLeft className="w-8 h-9 text-[#828282]" />
+        <SquareArrowLeft className="w-8 h-9 text-[#828282]" onClick={() => navigate('/')} />
       </div>
-      
+
       <div className='flex mt-9 gap-12 mx-20'>
         <div>
           <p className='text-[#828282] mb-4'>Gambar Produk</p>
-          <div className='flex flex-col items-center pt-14 pb-14 border-2 rounded-xl border-dashed outline-2 bg-slate-50 w-[270px] h-[270px]'>
-            {/* Jika ada gambar yang di-upload, tampilkan di dalam kotak */}
+          <div className=' flex flex-col items-center pt-14 pb-14 border-2 rounded-xl border-dashed outline-2 bg-slate-50 w-[270px] h-[270px]'>
             {image ? (
               <img src={image} alt="Preview" className="w-full h-full object-cover rounded-xl" />
             ) : (
@@ -34,22 +96,30 @@ const AddItem = () => {
                 <p className='text-[#828282] pt-5'>Unggah Gambar Produk</p>
               </>
             )}
-            <input type="file" onChange={handleImageUpload} className='text-[#828282] pt-5' />
+            <input type="file" onChange={handleImageUpload} className="hidden" id="file-upload" />
+            <label htmlFor="file-upload" className="cursor-pointer mt-5 text-green-600 hover:text-green-700">Pilih Gambar</label>
           </div>
-          <p className='text-[#828282] mt-5 text-[15px]'>Max 20 Mb, Format JPG & JPEG</p>
+          <p className='text-[#828282] mt-5 text-[15px] text-center'>Max 20 Mb, Format JPG & JPEG</p>
         </div>
-        
-        <div className='w-full '>
+
+        <div className='w-full'>
           <div className='flex gap-4 mb-5'>
-            {/* Input Nama Produk */}
             <div className='relative w-full'>
-              <input type="text" className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3" placeholder="Masukkan nama produk" />
+              <input 
+                type="text"
+                className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3"
+                placeholder="Masukkan nama produk"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
               <PackageOpenIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
             </div>
-            
-            {/* Select Jenis Sampah */}
+
             <div className='relative w-full'>
-              <select className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3 text-[#828282]">
+              <select
+                className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3 text-[#828282]"
+                value={type}
+                onChange={(e) => setType(e.target.value)}>
                 <option value="">Jenis sampah</option>
                 <option value="plastik">Plastik</option>
                 <option value="elektronik">Elektronik</option>
@@ -59,41 +129,62 @@ const AddItem = () => {
               <PackageSearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
             </div>
           </div>
-          
+
           <div className='flex gap-4'>
-            {/* Input Berat Produk */}
             <div className='relative w-full'>
-              <input type="text" className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3" placeholder="Masukkan berat produk (kg)" />
+              <input
+                type="text"
+                className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3"
+                placeholder="Masukkan berat produk (kg)"
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
+              />
               <AnvilIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
             </div>
-            
-            {/* Input Harga Produk */}
+
             <div className='relative w-full'>
-              <input type="text" className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3" placeholder="Masukkan harga produk" />
+              <input
+                type="text"
+                className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3"
+                placeholder="Masukkan harga produk"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
               <BadgePercent className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
             </div>
           </div>
+
           <div className='mt-5'>
             <textarea
-              className="w-full border border-gray-400 rounded-lg p-3  "
+              className="w-full border border-gray-400 rounded-lg p-3"
               placeholder="Masukkan deskripsi produk"
               value={description}
-              onChange={(e) => setDescription(e.target.value)} rows="10"></textarea>
+              onChange={(e) => setDescription(e.target.value)}
+              rows="10"
+            />
           </div>
+
           <div className='relative w-full mt-5'>
-              <input type="text" className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3" placeholder="Masukkan alamat" />
-              <MapPinned  className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
-            </div>
-            
-            <button className='bg-red-300 mt-5 w-full'>hahaha</button>
+            <input
+              type="text"
+              className="w-full border border-gray-400 rounded-lg p-2 pl-10 py-3"
+              placeholder="Masukkan alamat"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <MapPinned className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
+          </div>
+
+          <button
+            onClick={handleClick}
+            className='bg-green-700 mt-5 w-full rounded-lg py-3 text-white font-medium hover:bg-green-500'
+          >
+            Tambah Produk
+          </button>
         </div>
       </div>
-
-     
-
-
     </div>
   );
-}
+};
 
 export default AddItem;
--- src/Dashboard/index.jsx
+++ src/Dashboard/index.jsx
@@ -1,98 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Components/Navbar';
 import { useNavigate } from 'react-router-dom';
 import Latar from '../assets/Latar.jpeg';
-import { BatteryWarningIcon, LeafIcon, Milk, RecycleIcon, StickyNote } from 'lucide-react';
+import { BatteryWarningIcon, LeafIcon, Milk, Plus, RecycleIcon } from 'lucide-react';
+import axios from 'axios';
+
+
+
 
 const Home = () => {
+  const [products, setProducts] = useState([]); // Menambahkan state untuk produk
+  const navigasi = useNavigate();
 
-  const kategori = [
-    {id: 1,
-      jenis: "Plastik",
-      harga: 8.000,
-      namaToko: "Ecocycle"
-    },
-    {id: 2,
-      jenis: "Elektronik",
-      harga: 8.000,
-      namaToko: "Ecocycle"
-    },
-    {id: 3,
-      jenis: "Kaca",
-      harga: 8.000,
-      namaToko: "Ecocycle"
-    },
-    {id: 4,
-      jenis: "Tekstil",
-      harga: 8.000,
-      namaToko: "Ecocycle"
-    },
-  ];
+  // Fungsi untuk mengambil data produk dari API
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://6718b4807fc4c5ff8f4aabe4.mockapi.io/products');
+      console.log(response.data);
+      setProducts(response.data);  // Menyimpan data produk ke dalam state
+    } catch (error) {
+      console.error('Gagal mengambil data produk:', error);
+    }
+  };
 
-  const navigasi = useNavigate()
-  const handle = () =>{
-    navigasi("/tambah-produk")
+  // Mengambil data saat komponen pertama kali dirender
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  }
+  const handle = () => {
+    navigasi("/tambah-produk");
+  };
 
   return (
-
     <div>
       <Navbar />
 
       <div className='mx-20 mt-10 '>
-      <img className='w-full h-[500px] rounded-xl ' src={Latar} alt="Indonesia Tanpa Sampah" />
+        <img className='w-full h-[570px] rounded-xl ' src={Latar} alt="Indonesia Tanpa Sampah" />
       </div>
-      <div className='mx-20  h-[500px]'>
-        <div className='mt-10 '>
+
+      <div className='mx-20 h-[340px] '>
+        <div className='mt-10'>
           <h1 className='text-4xl font-medium text-slate-800'>Jenis Sampah</h1>
           <p className='mt-3 text-slate-600'>Lihat semua jenis sampah yang kami daur ulang.</p>
+          
         </div>
-       <div className='flex gap-20 mt-7 '>
+        <div className='flex gap-20 mt-7'>
           <div className=' flex flex-col justify-center items-center border w-72 h-48 transform hover:scale-105 hover:rotate transition-all rounded-lg hover:border-green-700'>
             <LeafIcon color="green" strokeWidth={3} className='flex justify-center' size={70} />
             <p className="text-xl mt-2 font-medium">Sampah Organik</p>
           </div>
 
-        <div className='flex flex-col justify-center items-center border w-72 h-48 transform hover:scale-105 hover:rotate transition-all rounded-lg hover:border-green-700'>
-          <Milk color="#add8e6" strokeWidth={3} className='flex justify-center' size={70}/>
-          <p className="text-xl mt-2 font-medium">Sampah Anorganik</p>
-        </div>
-        
-        <div className='flex flex-col justify-center items-center border w-72 h-48 transform hover:scale-105 hover:rotate transition-all rounded-lg hover:border-green-700'>
-          <BatteryWarningIcon color="#f4b42c" strokeWidth={3}  className='flex justify-center' size={70}/>
-          <p className="text-xl mt-2 font-medium">Sampah B3</p>
-        </div>
+          <div className='flex flex-col justify-center items-center border w-72 h-48 transform hover:scale-105 hover:rotate transition-all rounded-lg hover:border-green-700'>
+            <Milk color="#add8e6" strokeWidth={3} className='flex justify-center' size={70} />
+            <p className="text-xl mt-2 font-medium">Sampah Anorganik</p>
+          </div>
 
-        <div className='flex flex-col justify-center items-center border w-72 h-48 transform hover:scale-105 hover:rotate transition-all rounded-lg hover:border-green-700'>
-          <RecycleIcon color="gray" strokeWidth={3} className='flex justify-center' size={70}/>
-          <p className="text-xl mt-2 font-medium">Sampah Daur Ulang</p>
-        </div>
+          <div className='flex flex-col justify-center items-center border w-72 h-48 transform hover:scale-105 hover:rotate transition-all rounded-lg hover:border-green-700'>
+            <BatteryWarningIcon color="#f4b42c" strokeWidth={3} className='flex justify-center' size={70} />
+            <p className="text-xl mt-2 font-medium">Sampah B3</p>
+          </div>
 
-       
-        
+          <div className='flex flex-col justify-center items-center border w-72 h-48 transform hover:scale-105 hover:rotate transition-all rounded-lg hover:border-green-700'>
+            <RecycleIcon color="gray" strokeWidth={3} className='flex justify-center' size={70} />
+            <p className="text-xl mt-2 font-medium">Sampah Daur Ulang</p>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='flex justify-between mx-20'>
+        <div className=''>
+          <h1 className='text-4xl font-medium text-slate-800'>Jual Sampah, Selamatkan Bumi</h1>
+          <p className='mt-3 text-slate-600'>Dengan menjual sampah daur ulang, Anda berkontribusi untuk lingkungan yang lebih bersih dan hijau.</p>
+        </div>
+          <button onClick={handle} className="bg-green-700 flex items-center gap-2 rounded-lg p-4 text-white font-medium hover:bg-green-500">
+          <Plus />Tambah Produk
+        </button>
+        </div>
+      <div className="mx-20 mt-10  grid grid-cols-3 gap-4">
+        {products.map((product) => (
+          <div key={product.id} className="border border-gray-200 p-4 rounded-lg">
+            <img src={product.image} alt={product.name} className="w-full h-60 object-cover rounded-lg mb-4 transform hover:scale-105 hover:rotate transition-all" />
+            <h2 className="font-normal text-xl bg-green-50 w-[190px] rounded-2xl border-zinc-50">{product.name}</h2>
+            <p className="text-gray-600">{product.type}</p>
+            <p className="text-gray-600">{product.weight} kg</p>
+            <p className="text-gray-600">Rp {product.price}</p>
+            <p className="text-gray-600 mt-2">{product.description}</p>
+            <p className="text-gray-600 mt-2">{product.address}</p>
+          </div>
+        ))}
+      </div>
+
       
-       
-       
-       </div>
-
-      </div>
-      <div>
-        <button className='bg-red-800' onClick={handle} >Tambah</button>
-      </div>
-       <div className='flex gap-3'>
-          {kategori.map((data) => (
-            <div className='bg-slate-600' key={data.id} >
-            <p className='font-bold'>{data.jenis}</p>
-            <p>{data.harga}</p>
-            <p>{data.namaToko}</p>
-            </div>
-          ))}
-          
-      </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Home;
