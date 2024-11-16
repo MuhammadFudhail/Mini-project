d4d5232c5f7645878feee22d03383bcae9e908bb
Muhamad Fudhail <136212616+MuhammadFudhail@users.noreply.github.com>
2024-11-17 00:35:28 +0800

    melakukan erubahan di bagian navabr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,24 @@
 import React, { useState } from 'react';
 import logo from '../assets/logo.png';
-import { ShoppingCartIcon } from 'lucide-react';
 
 const Navbar = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    console.log('Searching for:', searchQuery);
-  };
-
   return (
     <div>
-      {/* Navbar */}
       <nav className="bg-white shadow-md" >
         <div className="flex justify-between items-center  py-1  mx-20">
-          {/* Logo */}
           <div className="flex items-center">
             <img src={logo} alt="Eco Logo" className="w-[80px] h-[80px]" />
-           
           </div>
 
-          {/* Menu Items */}
           <div className="flex items-center gap-20 text-gray-700">
             <a href="#about" className="hover:text-green-500">Tentang Kami</a>
-            
             <a href="#solutions" className="hover:text-green-500">Solusi AI</a>
-            
             <a href="#contact" className="hover:text-green-500">Kontak Kami</a>
-            <a href="#language" className="hover:text-green-500 flex items-center">
-           
-            </a>
+            <a href="#language" className="hover:text-green-500 flex items-center"></a>
           </div>
 
-          {/* Login Button */}
           <div className="flex items-center gap-4">
-            <ShoppingCartIcon className="h-6 text-gray-700" />
-            <a
-              href="/login"
-              className="text-white bg-green-500 px-4 py-2 rounded-md hover:bg-green-700"
-            >
-              Login
-            </a>
+            <a href="/login" className="text-white bg-green-500 px-4 py-2 rounded-md hover:bg-green-700">Login</a>
           </div>
         </div>
       </nav>
